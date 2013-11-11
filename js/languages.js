09430b3268c350b3893fb487539404911f3d74ba
macqueen <jessmacqueen@gmail.com>
2013-11-11 10:34:31 -0800

    append svg to container div

--- js/languages.js
+++ js/languages.js
@@ -1,3 +1,11 @@
-d3.csv("./repos_lang_month.csv")
+var margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select('.container').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom);
+
+d3.csv('./repos_lang_month.csv')
     .row(function(d) { return d; })
     .get(function(error, rows) { console.log(rows); });
